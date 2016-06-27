a0c82b0c7c5cd6a9c8a95d80a6a83ec382a9b1df
susandaiute <susandaiute@gmail.com>
2016-06-27 14:55:32 -0400

    Delete semicolon in pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   require 'net/http'
-  KEY = ENV['PETFINDER_KEY'];
+  KEY = ENV['PETFINDER_KEY']
   def index
     url = URI.parse('http://api.petfinder.com/pet.find?key=' + KEY + '&animal=dog&breed=Labrador+Retriever&output=full&location=02184&format=json&distance=100')
     req = Net::HTTP::Get.new(url.to_s)
